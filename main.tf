165ab604fc5b0ee49712d23030226b7eeac42404
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-30 15:44:37 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_db_instance" "rds_db" {
+  identifier           = var.identifier
+  engine               = var.engine
+  instance_class       = var.instance_class
+  username             = var.username
+  password             = var.password
+  allocated_storage    = var.storage
+  storage_type         = var.storage_type
+  publicly_accessible  = true
+  multi_az             = false
+  #db_subnet_group_name = "your-db-subnet-group"
+
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  #parameter_group_name = "default.mysql5.7"
+
+  final_snapshot_identifier = var.final_snapshot_identifier
+  skip_final_snapshot = false
+}
